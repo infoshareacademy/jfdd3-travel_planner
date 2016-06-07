c34d840d371cfb737f621e0c2d7a25c8f670a027
Lukasz <lukaszkisicki@yahoo.com>
2016-06-08 00:25:46 +0200

    test mapy

--- js/mapmarkers.js
+++ js/mapmarkers.js
@@ -2,24 +2,48 @@
  * Created by Lukasz on 2016-06-07.
  */
 $(document).ready(function () {
+    
+    function initialize() {
+        var mapProp = {
+            center:new google.maps.LatLng(54.3466531,18.6483953),
+            zoom:14,
+            mapTypeId:google.maps.MapTypeId.ROADMAP
+        };
+        var map=new google.maps.Map(document.getElementById("googleMap"), mapProp);
 
-    var places = [
+    }
+    google.maps.event.addDomListener(window, 'load', initialize);
+   
+    var $places = [
         {
             name: 'Fontanna Neptuna',
-            coordinates: [54.3485481, 18.6510408]
+            coordinates: {lat: 54.3485481, lng: 18.6510408}
         },
         {
             name: 'Żuraw Gadński',
-            coordinates: [54.3505653, 18.6553181]
+            coordinates: {lat: 54.3505653, lng: 18.6553181}
         },
         {
             name: 'Twierdza Wisłoujście',
-            coordinates: [54.3958477, 18.6769724]
+            coordinates: {lat: 54.3958477, lng: 18.6769724}
         },
         {
             name: 'Westerplatte',
-            coordinates: [54.4067082, 18.6658734]
+            coordinates: {lat: 54.4067082, lng: 18.6658734}
         }
-    ]
+    ];
+    console.log($places);
+
+    var $coordinates = $.map( $places, function(n){
+
+        return n.coordinates;
+    });
+    console.log($coordinates);
+
+    var $randomPlaces = $coordinates.forEach(function(element, index) {
+        return element[index].Math.round(Math.random) * 2;
+    });
+    console.log($randomPlaces);
     
 });
+
