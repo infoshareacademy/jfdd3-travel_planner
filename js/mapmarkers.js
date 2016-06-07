3e5ecbecfeead9062e74af9fbf2840c320548a18
Lukasz <lukaszkisicki@yahoo.com>
2016-06-07 21:51:35 +0200

    test mapy

---
+++ js/mapmarkers.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Lukasz on 2016-06-07.
+ */
+$(document).ready(function () {
+
+    var places = [
+        {
+            name: 'Fontanna Neptuna',
+            coordinates: [54.3485481, 18.6510408]
+        },
+        {
+            name: 'Żuraw Gadński',
+            coordinates: [54.3505653, 18.6553181]
+        },
+        {
+            name: 'Twierdza Wisłoujście',
+            coordinates: [54.3958477, 18.6769724]
+        },
+        {
+            name: 'Westerplatte',
+            coordinates: [54.4067082, 18.6658734]
+        }
+    ]
+    
+});
