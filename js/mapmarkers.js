21ae26e5b89641ce139a5413b0725a8612deba0a
Kisicki Łukasz <lukaszkisicki@yahoo.com>
2016-06-09 16:56:03 +0200

    css optimazing

--- js/mapmarkers.js
+++ js/mapmarkers.js
@@ -1,19 +1,6 @@
 /** Created by Lukasz on 2016-06-07.
  */
 $(document).ready(function () {
-
-    function initialize() {
-        var mapProp = {
-            center: new google.maps.LatLng(54.3466531, 18.6483953),
-            zoom: 14,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-
-    }
-
-    google.maps.event.addDomListener(window, 'load', initialize);
-
     var places = [
         {
             name: 'Fontanna Neptuna',
@@ -32,25 +19,65 @@ $(document).ready(function () {
             coordinates: {lat: 54.4067082, lng: 18.6658734}
         }
     ];
-    console.log(places);
 
     var coordinates = $.map(places, function (n) {
 
         return n.coordinates;
     });
-    console.log(coordinates);
 
     var randomPlaces = [];
+    var markers = [];
+    var map;
 
     for (var i = 0; i < 2; i += 1) {
 
         var startingPoints = coordinates[Math.round(Math.random() * 3)];
         randomPlaces.push(startingPoints);
-        console.log(startingPoints);
+        //console.log(startingPoints);
+    };
+
+    function initialize() {
+        var mapProp = {
+            center: new google.maps.LatLng(54.3466531, 18.6483953),
+            zoom: 10,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+         map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+
+
     }
 
+    function drop(){
+        //clearMarkers();
+        for(var i = 0; i< randomPlaces.length; i += 1) {
+            addMarkerTimeOut(randomPlaces[i], i + 1 * 500);
+        }
+    }
 
+    function addMarkerTimeOut(position, timeout) {
+        window.setTimeout(function(){
+            markers.push(new google.maps.Marker({
+                position: position,
+                map: map,
+                animation: google.maps.Animation.DROP
+            }));
+        },timeout);
+    }
+
+    google.maps.event.addDomListener(window, 'load', initialize);
+    drop();
+
+   // console.log(places);
+    //onsole.log(coordinates);
     console.log(randomPlaces);
+
+
+
+
+
+
+
+
 
 });
 
