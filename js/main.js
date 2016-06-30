c763fbff26ba2d1839ec9abf9cbbee870c9009c3
Karina Burchardt <karinaburchardt@gmail.com>
2016-06-30 14:38:33 +0200

    poprawione .top

--- js/main.js
+++ js/main.js
@@ -1,30 +1,51 @@
 //function for scrolling page to desired div element
 
-// $(function() {
-//     $('a[href*="#"]:not([href="#"])').click(function() {
-//         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-//             var target = $(this.hash);
-//             target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-//             if (target.length) {
-//                 $('html, body').animate({
-//                     scrollTop: target.offset().top
-//                 }, 1000,'swing',function() {
-//                     window.location.hash = target;
-//                     $(document).on("scroll", onScroll);
-//                 });
-//                 return false;
-//             }
-//         }
-//     });
-// });
+
 
 
 
 // set on function with event triggering onScroll function
 $(document).ready(function() {
-    // $(window).scroll(function() {
-    //   var window_top = $(window).scrollTop();
-    // })
+    var CurrentScroll = 0;
+    $(window).scroll(function(event){
+
+        var NextScroll = $(this).scrollTop();
+
+        if (NextScroll > CurrentScroll && CurrentScroll === 0){
+            $("#btn1").trigger('click');
+
+        }
+        else if (NextScroll < CurrentScroll && NextScroll === 0){
+            $("#btn2").trigger('click');
+        }
+        CurrentScroll = NextScroll;
+    });
+
+
+    $(function() {
+        $('a[href*="#"]:not([href="#"])').click(function() {
+            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+                if (target.length) {
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000,'swing',function() {
+                        window.location.hash = target;
+                        $(document).on("scroll", onScroll);
+                    });
+                    return false;
+                }
+            }
+        });
+    });
+
+
+
+
+    $(window).scroll(function() {
+        var window_top = $(window).scrollTop();
+    });
     $('.container-map').css({'height': (screen.height-160)+'px'});
     $('.fog-on-map').css({'height': (screen.height-160)+'px'});
     $('.main-slider-container').css({'height': (screen.height-160)+'px'});
@@ -61,12 +82,12 @@ $(document).ready(function() {
 
     // TODO opis wymagany
     function onScroll(){
-        var scrollPos = $(document).scrollTop();
+        var scrollPos = $(window).scrollTop();
 
-        $('a').each(function () {
+        $('a:not([href*="http"],[href="#"])').each(function () {
             var currLink = $(this);
             var refElement = $(currLink.attr("href"));
-            if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            if (refElement.position().top <= scrollPos && (refElement.position().top + refElement.height()) > scrollPos) {
                 $('a').removeClass("navigation-active");
                 currLink.addClass("navigation-active");
                 if (currLink.attr("href") === '#premiere') {$('.about-us-image').animate({opacity: 1},3000);}
@@ -76,4 +97,27 @@ $(document).ready(function() {
             }
         });
     }
+
+    $(document).keydown(function(e){
+        if (e.keyCode == 38) {
+            $("#btn2").trigger('click') ;
+        }else if (e.keyCode == 40) {
+            $("#btn1").trigger('click');
+        }
+    });
+
+
+    $("#btn1").click(function(){
+
+        $("#div1").animate({top: '-100%', left: '25%', opacity: '0', width: '50%'},{ duration: 3000, queue: false });
+
+
+    });
+
+    $("#btn2").click(function(){
+
+        $("#div1").animate({top: '0px', left: '0', opacity: '1', width: '100%'},{ duration: 3000, queue: false });
+
+
+    });
 });
